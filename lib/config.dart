8c03005c51c38834beea125786604aadff43987e
Galuu <galerdenechinbat@gmail.com>
2026-09-10 15:51:35 +0800

    chore: changed to prod

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
-// const presignedUrlEndpoint =
-//     'https://api.fuelmetrix.clix.mn/api/mobile/presigned-url';
-const presignedUrlEndpoint = 'http://192.168.1.23/api/mobile/presigned-url';
+const presignedUrlEndpoint =
+    'https://api.fuelmetrix.clix.mn/api/mobile/presigned-url';
+// const presignedUrlEndpoint = 'http://192.168.1.23/api/mobile/presigned-url';
 const demoClientId = 'BENZIN_CLIENT_ID';
 const demoClientSecret = 'BENZIN_CLIENT_SECRET';
 const demoName = 'Test User';
